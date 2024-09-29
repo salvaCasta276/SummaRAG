c8fdeff04f29bd350ff2b68b6a6912f10124ed10
vyeli <yeliv9527@gmail.com>
2024-09-28 21:07:43 -0300

    Different file output

--- Main/ainewscraper/ainewscraper/spiders/alignment.py
+++ Main/ainewscraper/ainewscraper/spiders/alignment.py
@@ -1,4 +1,5 @@
 import scrapy
+import os, json
 from ainewscraper.items import AinewscraperItem
 
 
@@ -17,8 +18,23 @@ class AlignmentSpider(scrapy.Spider):
         item = AinewscraperItem()
         
         item["url"] = response.url
-        item["title"] = ''.join(response.css('h1.PostsPageTitle-root a.PostsPageTitle-link ::text').getall()).strip(),
-        item["author"] = response.css('.PostsAuthors-authorName a.UsersNameDisplay-noColor::text').get(),
-        item["date"] = response.css('time::attr(datetime)').get(),
+        item["title"] = ''.join(response.css('h1.PostsPageTitle-root a.PostsPageTitle-link ::text').getall()).strip()
+        item["author"] = response.css('.PostsAuthors-authorName a.UsersNameDisplay-noColor::text').get()
+        item["date"] = response.css('time::attr(datetime)').get()
         item["content"] = ' '.join(response.css('#postContent .InlineReactSelectionWrapper-root ::text').getall()).strip()
+
+        self.write_json(item)
         yield item
+
+    def write_json(self, item):
+        # Create a directory for the output if it doesn't exist
+        if not os.path.exists('output'):
+            os.makedirs('output')
+
+        # Create a filename based on the title extract from the url
+        title = item["url"].split('/')[-1]
+
+        # Write the item to a json file
+        with open(f'output/{title}.json', 'w') as f:
+            json.dump(dict(item), f, indent=4)
+        
